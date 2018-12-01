2cf341c062bd0210146939f67f8b26adc115a731
JoeC <josephcrowley85@gmail.com>
2018-12-01 22:44:19 +1100

    homepage created in jobs app views using bootstrap Album example

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Blog(models.Model):
-    title = models.CharField(max_length = 30)
+    title = models.CharField(max_length = 255)
     pub_date = models.DateTimeField(default=timezone.now)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
